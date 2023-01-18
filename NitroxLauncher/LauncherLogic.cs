e152ca4e35caf1f3fb5764ec54cd369660395859
Papela <45976195+Papela@users.noreply.github.com>
2023-01-18 19:08:25 +0100

    Removed unecessary codes

--- NitroxLauncher/LauncherLogic.cs
+++ NitroxLauncher/LauncherLogic.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.ComponentModel;
 using System.IO;
 using System.Reflection;
@@ -112,7 +112,7 @@ namespace NitroxLauncher
 
             lastFindSubnauticaTask = Task.Factory.StartNew(() =>
             {
-                PirateDetection.TriggerOnDirectory(path);
+                //PirateDetection.TriggerOnDirectory(path);
 
                 if (!FileSystem.Instance.IsWritable(Directory.GetCurrentDirectory()) || !FileSystem.Instance.IsWritable(path))
                 {
@@ -192,11 +192,6 @@ namespace NitroxLauncher
             {
                 NavigateTo<OptionPage>();
                 throw new Exception("Location of Subnautica is unknown. Set the path to it in settings.");
-            }
-
-            if (PirateDetection.HasTriggered)
-            {
-                throw new Exception("Aarrr! Nitrox walked the plank :(");
             }
 
 #if RELEASE
